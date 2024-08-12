61da78745ad91cf7f54356c71d76222a992440c5
luuib <129198298+luuib@users.noreply.github.com>
2024-08-12 09:18:12 -0300

    Add files via upload

---
+++ pilhas.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __int__(self):
+        self.items = []
+
+    def isEmpyt(self):
+        return self.items == []
+    
+    def push(self, items):
+        self.items.append(items)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+    
+    def size(self):
+        return len(self.items)
+    
+pilha = Stack()
+
+pilha.push('A')
+pilha.push('B')
+pilha.push('C')
+pilha.push('D')
+pilha.push('E')
+pilha.push('F')
+print(pilha.items)
+print(pilha.peek())
+print(pilha.items)
+pilha.pop()
+print(pilha.items)
